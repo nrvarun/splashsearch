2f0d2de3f8616d757067b2208d6761069fc58a63
Varun <varun@lollypop.design>
2020-03-20 04:27:09 +0530

    emergence.js removed as it was causing issues

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import React, { useEffect } from "react";
-import emergence from "emergence.js";
 
 import "./sass/main.scss";
 
 import Home from "./components/home";
 
 function App() {
-  useEffect(() => {
-    emergence.init({
-      reset: false,
-      handheld: true
-    });
-  });
-
   return (
     <div className="App">
       <Home />
