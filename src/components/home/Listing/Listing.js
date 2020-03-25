c3ac3084bd5c759f2161918460562c87d503c7c7
Varun <varun@lollypop.design>
2020-03-25 20:23:28 +0530

    some changes

--- src/components/home/Listing/Listing.js
+++ src/components/home/Listing/Listing.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ListingItem from "./ListingItem";
-import { FixedSizeGrid } from "react-window";
 
 const Listing = props => {
   const { results, keyword } = props;
